cbe0b9677fea9aa92b48aec856438170f1f7ae0f
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2019-11-01 16:21:58 -0400

    added display results functions

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 
 
-// const url = 'developer.nps.gov/api/v1/parks';
+const searchURL = 'https://developer.nps.gov/api/v1/parks';
 
-// const apiKey = 'TZe9rx6fNdE3DBoYdU9dMgRXdoy9heOItmMyXdrW';
+const apiKey = 'TZe9rx6fNdE3DBoYdU9dMgRXdoy9heOItmMyXdrW';
 
 // fetch!
 // return visual rep of results - creating html to display
 // watch button
 
-const fetchParks = function(url) {
-  const apiKey = 'TZe9rx6fNdE3DBoYdU9dMgRXdoy9heOItmMyXdrW';
+const getParks = function(parkChoice, maxResults=10){
 
-  fetch ('url')
+const params = {
+  key: apiKey,
+  q: parkChoice,
+  maxResults,
+}
+
+const queryString = formatQueryParams(params)
+const url = searchURL  + '?' + queryString
+console.log(url);
+
+fetch (url)
     .then(response => {
       if(response.ok){
         return response.json();
@@ -22,11 +31,29 @@ const fetchParks = function(url) {
 }
 
 
+const displayResults = function(responseJson){
+  $('.list').empty()
+
+  for(let i = 0; i < responseJson.data.length; i++){
+    $('.list').append(`
+<li>${responseJson.data[i].addresses.line2}</li>
+<li>${responseJson.data[i].directionsUrl}</li>
+<li>${responseJson.data[i].description}</li>
+`)
+  }
+
+}
+
+
+
+
+
 const watchButton = function() {
   $('#main-form').submit(e => {
     e.preventDefault;
     let parkChoice = $('#park-input').val();
     let maxResults = $('#num-of-results').val();
     getParks(parkChoice, maxResults);
+    
   });
 }
